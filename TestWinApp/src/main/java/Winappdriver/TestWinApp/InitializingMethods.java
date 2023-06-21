535acaee26c6a4c3a6c8ec52476027180d948520
AshokMarrapu <AshokMarrapu@DESKTOP-BK67L73>
2023-06-21 17:46:51 +0530

    commit

--- TestWinApp/src/main/java/Winappdriver/TestWinApp/InitializingMethods.java
+++ TestWinApp/src/main/java/Winappdriver/TestWinApp/InitializingMethods.java
@@ -1,18 +1,41 @@
 package Winappdriver.TestWinApp;
 
+import java.awt.Desktop;
+import java.io.*;
 import java.net.URL;
 import java.util.Set;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 import io.appium.java_client.windows.WindowsDriver;
 
 public class InitializingMethods {
 
+	
 	public static WindowsDriver driver;
 
 
-
+	@BeforeTest
 	public static WindowsDriver Setup() throws Exception {
-
+		
+//		/*
+//		 * String WADServerPath =
+//		 * "C:\\Program Files\\Windows Application Driver\\WinAppDriver.exe"; String
+//		 * command = WADServerPath; ProcessBuilder builder = new
+//		 * ProcessBuilder(command); Process process = builder.start();
+//		 */	
+//		
+//		
+//			//constructor of file class having file as argument  
+//			File file = new File("C:\\Program Files\\Windows Application Driver\\WinAppDriver.exe");   
+//			Desktop desktop = Desktop.getDesktop();
+//			desktop.open(file);
+//			System.out.println("WinAppDriver Server is started");
+//			Thread.sleep(3000);		
+		
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability("app", "C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE");
 		capabilities.setCapability("platformName", "Windows");
@@ -22,18 +45,18 @@ public class InitializingMethods {
 		System.out.println("Outlook is Launched");
 		Set<String> allWindowHandles = driver.getWindowHandles();
 		driver.switchTo().window(allWindowHandles.iterator().next());
+		System.out.println("Outlook is Launched @Before Test");
 		return driver;
-
 	}
 
-
+	@AfterTest
 	public static void Close() {
 
 		driver.findElementByXPath(
 				"//*[@Name='Ribbon'][@ClassName='NetUInetpane']/Button[@Name='Close'][@ClassName='NetUIAppFrameHelper']")
 				.click();
 		// Thread.sleep(1000);
-		System.out.println("Outlook is closed");
+		System.out.println("Outlook is closed @After Test");
 	}
 
 }
--- TestWinApp/src/test/java/Winappdriver/TestWinApp/TestOneOutlookLaunch.java
+++ TestWinApp/src/test/java/Winappdriver/TestWinApp/TestOneOutlookLaunch.java
@@ -1,19 +1,25 @@
 package Winappdriver.TestWinApp;
 import java.util.Set;
+import org.testng.annotations.Test;
+
 
 public class TestOneOutlookLaunch extends InitializingMethods {
  
-	
+	@Test
 	public static void Launch() throws Throwable {
 
+		
 		//WindowsDriver driver = (WindowsDriver) Setup();
 		Set<String> allWindowHandles = driver.getWindowHandles();
 		driver.switchTo().window(allWindowHandles.iterator().next());
+		
 		Thread.sleep(1000);		
 	}
 
 	
 	public static void main(String[] args) throws Throwable {
+
+		
 		Setup();
 		Launch();
 		Close();
@@ -22,3 +28,4 @@ public class TestOneOutlookLaunch extends InitializingMethods {
 
 	}
 }
+
